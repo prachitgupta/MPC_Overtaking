6d33c894b249b506fb12c7ad87fe128c87c825e5
prachitgupta <prachitgupta100@gmail.com>
2024-04-16 12:15:02 +0530

    commented templalate don't bother understanding now

--- tempelate.py
+++ tempelate.py
@@ -3,23 +3,23 @@
 
 import casadi as ca
 import casadi.tools as ca_tools
-
+import pickle
 import numpy as np
 import time
 from draw import Draw_MPC_point_stabilization_v1
 
 
-def shift_movement(T, t0, x0, u, x_f, f):
-    f_value = f(x0, u[0, :])
-    st = x0 + T*f_value.full()
-    t = t0 + T
+def shift_movement(T, t0, x0, u, x_f, f): ##time step, cureent time,current pos, control prediction,state prediction and dynamics
+    f_value = f(x0, u[0, :]) ##Xdot  ##exected only once
+    st = x0 + T*f_value.full() ##update
+    t = t0 + T ##update time
     # print(u[:,0])
     # u_end = np.concatenate((u[:, 1:], u[:, -1:]), axis=1)
-    u_end = np.concatenate((u[1:], u[-1:]))
+    u_end = np.concatenate((u[1:], u[-1:])) #shifts all elements of u one position to the left(as first executed), and the last element wraps around to the beginning.
     # x_f = np.concatenate((x_f[:, 1:], x_f[:, -1:]), axis=1)
     x_f = np.concatenate((x_f[1:], x_f[-1:]), axis=0)
 
-    return t, st, u_end, x_f
+    return t, st, u_end, x_f ##st is precisly what i needed
 
 
 if __name__ == '__main__':
@@ -29,7 +29,11 @@ if __name__ == '__main__':
     ##control limits
     v_max = 0.6
     omega_max = np.pi/4.0
-
+    
+    ##load lookup table
+    with open("lookup_table2.pkl", "rb") as f:
+        lookup_table_loaded = pickle.load(f)
+        
                                 ##define state
     #z = MX.sym("state_vector",4,1)  ##not specifically designed for numeric computation
     #u = MX.sym("control_v",2,1)
@@ -54,11 +58,12 @@ if __name__ == '__main__':
     controls = ca.vertcat(v, omega)
     n_controls = controls.size()[0]
 
-    # rhs
+    ###dynamics how they will propgate
     rhs = ca.vertcat(v*ca.cos(theta), v*ca.sin(theta))
     rhs = ca.vertcat(rhs, omega)
 
     # function
+    ##f(x,u) can be non linear
     f = ca.Function('f', [states, controls], [rhs], [
                     'input_state', 'control_input'], ['rhs'])
 
@@ -67,7 +72,7 @@ if __name__ == '__main__':
 
     X = ca.SX.sym('X', n_states, N+1)
 
-    P = ca.SX.sym('P', n_states+n_states)
+    P = ca.SX.sym('P', n_states+n_states) ##initial state and final state param 
 
     # define
     Q = np.array([[1.0, 0.0, 0.0], [0.0, 5.0, 0.0], [0.0, 0.0, .1]])
@@ -76,13 +81,15 @@ if __name__ == '__main__':
     obj = 0  # cost
     g = []  # equal constrains
     
-    g.append(X[:, 0]-P[:3])
+    g.append(X[:, 0]-P[:3]) ##initial condition
     for i in range(N):
         ##import value function somehow
         
+        ##obj penalizes deviation from final state (soften) and control
         obj = obj + ca.mtimes([(X[:, i]-P[3:]).T, Q, X[:, i]-P[3:]]
                               ) + ca.mtimes([U[:, i].T, R, U[:, i]])
-        x_next_ = f(X[:, i], U[:, i])*T + X[:, i]
+        ##dynamics
+        x_next_ = f(X[:, i], U[:, i])*T + X[:, i] ##xt+1 = xdot * T + xt
         g.append(X[:, i+1]-x_next_)
     
     ##define solver
@@ -119,7 +126,7 @@ if __name__ == '__main__':
     t0 = 0.0
     ##initial state
     x0 = np.array([0.0, 0.0, 0.0]).reshape(-1, 1)  # initial state
-    x0_ = x0.copy()
+    x0_ = x0.copy() ##fixed
     ##store next states 
     x_m = np.zeros((n_states, N+1))
     next_states = x_m.copy().T
@@ -140,30 +147,44 @@ if __name__ == '__main__':
     index_t = []
     # inital test
 
-    while(np.linalg.norm(x0-xs) > 1e-2 and mpciter-sim_time/T < 0.0):
+    while(np.linalg.norm(x0-xs) > 1e-2 and mpciter-sim_time/T < 0.0): ##how much accuracy in reaching goal
         # set parameter
-        c_p = np.concatenate((x0, xs))
+        c_p = np.concatenate((x0, xs)) ##parameter storing initial and final state (initial updates and final fixed)
         init_control = np.concatenate((u0.reshape(-1, 1), next_states.reshape(-1, 1)))
         t_ = time.time()
+        
+        ##called MPC solver 
+        ##Todo this is prediction horizon
+        ##x0 and hence c_p updates
         res = solver(x0=init_control, p=c_p, lbg=lbg,
                      lbx=lbx, ubg=ubg, ubx=ubx)
         index_t.append(time.time() - t_)
         # the feedback is in the series [u0, x0, u1, x1, ...]
-        estimated_opt = res['x'].full()
+        estimated_opt = res['x'].full() ##all otimal variables
+        ##entire horizon optimal control sequence and states prediction returned  (check shapes)
         u0 = estimated_opt[:200].reshape(N, n_controls)  # (N, n_controls)
         x_m = estimated_opt[200:].reshape(N+1, n_states)  # (N+1, n_states)
-        x_c.append(x_m.T)
-        u_c.append(u0[0, :])
+        
+        ##history upddates
+        x_c.append(x_m.T) ##transpose state sorted, entire predicted trajectory stored
+        u_c.append(u0[0, :])  ##only first control stored
         t_c.append(t0)
-        t0, x0, u0, next_states = shift_movement(T, t0, x0, u0, x_m, f)
+        
+        ##todo add control horizon
+        ##update state
+        t0, x0, u0, next_states = shift_movement(T, t0, x0, u0, x_m, f) ##entire prediction horizon for contol
+        ##u0 , next states are predictions excluding first
+        ##update initial states which are actually executed
         x0 = ca.reshape(x0, -1, 1)
         x0 = x0.full()
-        xx.append(x0)
+        xx.append(x0) ##this is precisley how he moves
         # print(u0[0])
         mpciter = mpciter + 1
     t_v = np.array(index_t)
     print(t_v.mean())
     print((time.time() - start_time)/(mpciter))
+    
+    ##to do add plots for visulation (best case trajecory on a heat map)
 
     draw_result = Draw_MPC_point_stabilization_v1(
         rob_diam=0.3, init_state=x0_, target_state=xs, robot_states=xx)
